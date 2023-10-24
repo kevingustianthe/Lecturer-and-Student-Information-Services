6e891e4b6282b6b4c6a8998a91cb3c27d3b72b45
kevingustianthe <gustiankevin@gmail.com>
2023-10-24 20:48:17 +0700

    feat: Add query method findFirstByToken to AccountRepository

--- src/main/java/com/fikupnvj/restfulapi/repository/AccountRepository.java
+++ src/main/java/com/fikupnvj/restfulapi/repository/AccountRepository.java
@@ -1,2 +1,13 @@
+package com.fikupnvj.restfulapi.repository;
+
+import com.fikupnvj.restfulapi.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, String> {
 
     Optional<Account> findFirstByToken(String token);
+}
