75e8503ea1125c39939b7e34f596cac98af65271
kevingustianthe <gustiankevin@gmail.com>
2023-10-24 20:36:56 +0700

    feat: Add Account entity

---
+++ src/main/java/com/fikupnvj/restfulapi/entity/Account.java
@@ -0,0 +1,32 @@
+package com.fikupnvj.restfulapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tb_m_account")
+public class Account {
+
+    @Id
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String role;
+
+    private Boolean status;
+
+    @Column(name = "verification_code")
+    private String verificationCode;
+
+    private String token;
+
+    @Column(name = "token_expired_at")
+    private Long tokenExpiredAt;
+}
