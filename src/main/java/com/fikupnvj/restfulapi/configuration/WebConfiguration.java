9cc9afee21f1e1407ba9dfc734b021f8abbb80ec
kevingustianthe <gustiankevin@gmail.com>
2023-11-05 11:09:57 +0700

    feat: Added role Student and Lecturer for Authorization and Authentication

--- src/main/java/com/fikupnvj/restfulapi/configuration/WebConfiguration.java
+++ src/main/java/com/fikupnvj/restfulapi/configuration/WebConfiguration.java
@@ -1,6 +1,8 @@
 package com.fikupnvj.restfulapi.configuration;
 
 import com.fikupnvj.restfulapi.resolver.AdminArgumentResolver;
+import com.fikupnvj.restfulapi.resolver.LecturerArgumentResolver;
+import com.fikupnvj.restfulapi.resolver.StudentArgumentResolver;
 import com.fikupnvj.restfulapi.resolver.UserArgumentResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -18,10 +20,18 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Autowired
     private AdminArgumentResolver adminArgumentResolver;
 
+    @Autowired
+    private LecturerArgumentResolver lecturerArgumentResolver;
+
+    @Autowired
+    private StudentArgumentResolver studentArgumentResolver;
+
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         WebMvcConfigurer.super.addArgumentResolvers(resolvers);
         resolvers.add(adminArgumentResolver);
         resolvers.add(userArgumentResolver);
+        resolvers.add(lecturerArgumentResolver);
+        resolvers.add(studentArgumentResolver);
     }
 }
---
+++ src/main/java/com/fikupnvj/restfulapi/resolver/LecturerArgumentResolver.java
@@ -0,0 +1,55 @@
+package com.fikupnvj.restfulapi.resolver;
+
+import com.fikupnvj.restfulapi.entity.Account;
+import com.fikupnvj.restfulapi.repository.AccountRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Objects;
+
+@Component
+public class LecturerArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterType().equals(Account.class) && Objects.equals(parameter.getParameterName(), "lecturer");
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest servletRequest = (HttpServletRequest) webRequest.getNativeRequest();
+        String token = servletRequest.getHeader("X-API-TOKEN");
+
+        if (token == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Token not found");
+        }
+
+        Account account = accountRepository.findFirstByToken(token)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Unauthorized"));
+
+        if (!account.getStatus()) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Account not verified");
+        }
+
+        if (account.getTokenExpiredAt() < System.currentTimeMillis()) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Token expired");
+        }
+
+        if (!Objects.equals(account.getRole(), "LECTURER")) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Account doesn't have access");
+        }
+
+        return account;
+    }
+}
