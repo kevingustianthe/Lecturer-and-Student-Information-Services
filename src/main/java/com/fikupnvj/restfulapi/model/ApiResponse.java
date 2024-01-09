746109bd8023fe1720c07c5115fef4973749c8d0
kevingustianthe <gustiankevin@gmail.com>
2024-01-09 18:36:47 +0700

    refactor: Update variable name from `success` to `status`

--- src/main/java/com/fikupnvj/restfulapi/model/ApiResponse.java
+++ src/main/java/com/fikupnvj/restfulapi/model/ApiResponse.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ApiResponse<T> {
-    boolean success;
+    boolean status;
     String message;
     T data;
 }
